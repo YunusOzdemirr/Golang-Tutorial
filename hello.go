d1e54e8b3b12d0a7e3813d929ac3b0b7a7dfe0c5
yunusozdemirr <yunusozdemir468@gmail.com>
2022-06-25 14:04:19 +0300

    Channel With GoRoutine

--- hello.go
+++ hello.go
@@ -8,7 +8,18 @@ import (
 )
 
 func main() {
-	GoRoutines()
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
+	channel := make(chan int)
+	go Sum(numbers, channel)
+	x := <-channel
+	fmt.Println(x)
+}
+func Sum(numbers []int, c chan int) {
+	sum := 0
+	for _, value := range numbers {
+		sum += value
+	}
+	c <- sum
 }
 
 func GoRoutines() {
